d8fc11580e496813ca66cf3da06775c92eea3e20
wahibhanii <wahib.hanii@gmail.com>
2017-12-21 21:41:10 +0700

    add create podcast

--- controllers/podcasts-controller.js
+++ controllers/podcasts-controller.js
@@ -2,11 +2,40 @@
 
 // wahib
 // zuhri
+const Podcast = require('../models/podcast')
 
 class PodcastsController{
 
   static createPodcast(req, res){
+    // method upload dari multer dkk
+    // uploadPodcast()
+    // .then(result => {
 
+      // Database related process starts here
+      let newPodcast = {
+        caster  : req.body.userId,
+        // audioUrl: result.audioUrl, //atau apapun keluaran result
+        title   : req.body.title,
+        duration: req.body.duration,
+        likers  : [] // initial value should be empty, no liker yet
+      }
+      Podcast.create(newPodcast)
+      .then(result => {
+        res.status(200).jason({
+          message : 'Create new podcast successful...',
+          data: result
+        })
+      })
+      .catch(err => {
+        console.log(err),
+        res.status(500).send(err)
+      })
+      
+
+    // })
+
+   
+    
   }
 
   static deletePodcast(req, res){
